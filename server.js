be19b43e806bdb7c2437a5eea032a67a8c192e7f
Przemysław Piotrowski <przemyslaw.piotrowski@polcode.net>
2018-07-12 14:31:00 +0200

    Modify server port to use environment variable PORT Redirect https to http

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((request, response, next) => {
+  if (request.headers['x-forwarded-proto'] == 'http') {
+    next();
+  } else {
+    response.redirect('http://' + request.hostname + request.url);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+app.listen(PORT, function() {
+  console.log("Server is running on port " + PORT);
 })
