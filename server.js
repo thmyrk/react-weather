96a70ab3d69de6021e93e99e03afa7206afffe65
Przemysław Piotrowski <przemyslaw.piotrowski@polcode.net>
2018-07-20 12:46:21 +0200

    Enable https

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use((request, response, next) => {
-  if (request.headers['x-forwarded-proto'] == 'https') {
-    response.redirect('http://' + request.hostname + request.url);
-  } else {
-    next();
-  }
-});
-
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
