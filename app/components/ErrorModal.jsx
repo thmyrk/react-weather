4f34f1a3c1767372660f1a27ce40beda4db1d43a
Przemysław Piotrowski <przemyslaw.piotrowski@polcode.net>
2018-07-24 10:31:22 +0200

    Linting fixes in ErrorModal Fix ErrorModal styling

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -2,18 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactDOMServer from 'react-dom/server'
 import PropTypes from 'prop-types'
-import createReactClass from 'create-react-class'
 
 class ErrorModal extends React.Component {
-  getDefaultProps() {
-    return {
-      title: 'Error'
-    };
-  }
-  componentDidMount() {
-    var {title, message} = this.props;
+  componentDidMount () {
+    var {title, message} = this.props
     var modalMarkup = (
-      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+      <div id="error-modal" className="error-modal reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
         <p>
@@ -22,25 +16,28 @@ class ErrorModal extends React.Component {
           </button>
         </p>
       </div>
-    );
+    )
 
-    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
-    $(ReactDOM.findDOMNode(this)).html($modal);
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup))
+    $(ReactDOM.findDOMNode(this)).html($modal)
 
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
+    var modal = new Foundation.Reveal($('#error-modal'))
+    modal.open()
   }
-  render() {
+  render () {
     return (
       <div>
       </div>
-    );
+    )
   }
 };
-
+ErrorModal.defaultProps = {
+  title: 'Error',
+  message: 'something went wrong'
+}
 ErrorModal.propTypes = {
   title: PropTypes.string,
   message: PropTypes.string.isRequired
 }
 
-export default ErrorModal;
+export default ErrorModal
