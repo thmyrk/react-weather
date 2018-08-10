afb4cba0d0e3d6772d1adb11a62f27423a3371f9
thmyrk <przemyslaw.piotrowski@atteo.com>
2018-08-10 13:21:35 +0200

    Add github link to footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <span className="align-middle">Przemysław Piotrowski &copy; 2018</span>
+      <span className="align-middle">
+        developed by Przemysław Piotrowski &copy; 2018
+        <a href="https://github.com/thmyrk">
+          <img className="github_icon" src={require("../styles/images/github_icon.svg")} alt="Github" />
+        </a>
+      </span>
     </footer>
   )
 }
